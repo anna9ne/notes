aa6ded6909ed4fb1de7043e592bb093fc722791f
anna <anna9ne@gmail.com>
2025-02-19 22:43:23 +0300

    Create Blade files for CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::get('/notes/{id}', [NoteController::class, 'show'])->name('notes.show');
+Route::get('/notes/{id}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+Route::put('/notes/{id}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{id}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
