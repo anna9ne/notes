e319e13e514564d72e4d671adb85bf4f641f5363
anna <anna9ne@gmail.com>
2025-02-20 21:34:21 +0300

    Include CSS and JS Files

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,3 +1,22 @@
 <x-layout>
-    <h1>index</h1>
+    <div class="note-container">
+        <a href="{{ route('notes.create') }}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+                    </div>
+                    <div class="note-bottons">
+                        <a href="{{ route('notes.show', $note) }}" class="note-edit-button">View</a>
+                        <a href="{{ route('notes.edit', $note) }}" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
 </x-layout>
