1afbc75cc2cde009f730f9137630ebae508b277a
anna <anna9ne@gmail.com>
2025-02-22 14:10:38 +0300

    Adjust Navigation Links

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="note-container">
+    <div class="note-container py-12">
         <a href="{{ route('notes.create') }}" class="new-note-btn">
             New Note
         </a>
@@ -23,6 +23,9 @@
 
         </div>
 
-        {{ $notes->links() }}
+        <div class="p-6">
+            {{ $notes->links() }}
+        </div>
+
     </div>
 </x-app-layout>
